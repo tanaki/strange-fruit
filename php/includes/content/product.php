42135ccea5a2a35e2ff2c714c721a4961115c91d
Nicolas Pigelet <nipigelet@gmail.com>
2012-07-01 18:06:05 +0100

    more work on collection

--- php/includes/content/product.php
+++ php/includes/content/product.php
@@ -1,2 +1,19 @@
-product
-
+<h1>
+	Silver Collection
+</h1>
+<h2>
+	Silver Bear Plate
+</h2>
+<div class="details">
+	<div class="desc">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+	</div>
+	<div class="price">£200</div>
+	<ul class="list-details">
+		<li>Ring</li>
+		<li>Available sizes</li>
+		<li>Bespoke</li>
+		<li>Material</li>
+	</ul>
+	<a class="btn add-cart" href="#">Add to cart</a>
+</div>
--- php/includes/template.php
+++ php/includes/template.php
@@ -6,6 +6,9 @@
 		<title><?php echo $title; ?>Strange Fruit - Jewellery brand based in London</title>
 		<!-- TODO Add meta tags -->
 
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+		<!-- <link href='http://fonts.googleapis.com/css?family=Yanone+Kaffeesatz:400,300,700,200&subset=latin,latin-ext' rel='stylesheet' type='text/css'> -->
+
 		<!--[if lt IE 9]>
 		<script src="js/libs/html5shiv.js"></script>
 		<![endif]-->
